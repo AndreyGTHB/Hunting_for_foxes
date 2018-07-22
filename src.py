4149e520776ebffc75e3c5f1e9fc704151266794
Grigory <gtukmachev@gmail.com>
2018-07-22 18:16:45 +0700

    error not found

--- src.py
+++ src.py
@@ -1,3 +1,4 @@
+#Code of the program.
 from random import randint
 field = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 randomX = randint (0, 9)
@@ -8,3 +9,32 @@ for n in range (foxes):
         randomX = randint (0, 9)
         randomY = randint (0, 9)
     field [randomX][randomY] = 1
+while (foxes != 0):
+    x = input ('x - coordinate:')
+    y = input ('y - coordinate:')
+    x_int = int (x)
+    y_int = int (y)
+    see = 0
+    if (field [x_int][y_int] == 1):
+        print ('FOX!!!')
+        foxes -= 1
+        continue
+    else:
+        for i in range (len (field)):
+            if (field [i][y_int] == 1):
+                see += 1
+            if (field [x_int][i] == 1):
+                see += 1
+        while (x_int >= 0 and y_int >= 0):
+            if (field [x_int][y_int] == 1):
+                see += 1
+            x_int -= 1
+            y_int -= 1
+        x_int += 1
+        y_int += 1
+        while (x_int <= 9 and y_int <= 9):
+            if (field [x_int][y_int] == 1):
+                see += 1
+                x_int += 1
+                y_int += 1
+    print ('See:', end = see)
